779f0e2c74aaff97286706b1d6ffe0de00ca3146
gothog-dev <belko1801@gmail.com>
2026-01-11 15:03:51 +0200

    fix space line 19

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ marks = {
 }
 collection_of_coins = {1, 2, 25}
 sorted_variables = {
-    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info], 
+    "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
     "mutable": [my_favourite_films, marks, collection_of_coins]
 }
 # write your code here
