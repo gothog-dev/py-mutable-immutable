096fc9c977261dba014bb7c1206fc9aed42454c6
gothog-dev <belko1801@gmail.com>
2026-01-11 15:08:21 +0200

    fix comment idk why

--- app/main.py
+++ app/main.py
@@ -19,4 +19,3 @@ sorted_variables = {
     "immutable": [lucky_number, pi, one_is_a_prime_number, name, profile_info],
     "mutable": [my_favourite_films, marks, collection_of_coins]
 }
-# write your code here
